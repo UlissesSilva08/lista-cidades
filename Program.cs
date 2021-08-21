7002e7be7d42462e09af2c6eaa636ba389ead61a
UlissesSilva08 <ulissesmasterchief@gmail.com>
2021-08-20 21:25:18 -0300

    Adicionando as cidades do litoral no comando WriteLine

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace lista_cidades
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Lista das Cidades do Litoral Sul no\nestado de São Paulo:");
+            Console.WriteLine("Bertioga");
+            Console.WriteLine("Cubatão");
+            Console.WriteLine("Santos");
+            Console.WriteLine("Guarujá");
+            Console.WriteLine("São Vicente");
+            Console.WriteLine("Praia Grande");
+            Console.WriteLine("Mongaguá");
+            Console.WriteLine("Itanhaém");
+            Console.WriteLine("Peruíbe");
+        }
+    }
+}
